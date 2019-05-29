bee4a9b1b3b7651536702ee8cf782731531dcec2
github username <github email address>
2019-05-28 18:50:18 -0700

    merging my latest with master so Jenny's changes can be merged

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,7 +7,7 @@ require 'pry'
 def get_word_from_api(word)
   what_to_look_for = 'http://api.urbandictionary.com/v0/define?term={word}'
   response_hash = JSON.parse(RestClient.get(what_to_look_for))
-  r#esults = response_hash["results"]
+  #r#esults = response_hash["results"]
 
   binding.pry
  end
