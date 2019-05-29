fd08ed7c9f2b208dce5d0b65cd744e451640564b
github username <github email address>
2019-05-29 09:35:29 -0700

    client stuff

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require 'pry'
 
 #turn off debug msgs
 old_logger = ActiveRecord::Base.logger
@@ -13,4 +14,12 @@ system('clear')
 welcome
 #process_user
 word = process_word_query
+
 get_word_from_api(word)
+
+binding.pry
+
+
+sleep(1)
+system('clear')
+#results
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,15 +2,9 @@
 require 'json'
 require 'pry'
 
-#word = "dank"
 
 def get_word_from_api(word)
   what_to_look_for = 'http://api.urbandictionary.com/v0/define?term={word}'
   response_hash = JSON.parse(RestClient.get(what_to_look_for))
   #r#esults = response_hash["results"]
-
-  binding.pry
  end
-# #http://api.urbandictionary.com/v0/define?term={word}
-# 'http://api.urbandictionary.com/v0/define?term={"dank"}'
-# http://api.urbandictionary.com/v0/define?term={dank}
